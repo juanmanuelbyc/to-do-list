cab6521b783a8210b179d1ef2c22e4ff1ddfc7a5
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-10 11:20:36 -0400

    set up web pack server and show object data

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import './style.css';
+
+const tasks = [
+  {
+    description: "cleaning",
+    completed: false,
+    index: 0,
+    },
+  {
+    description: "accounting",
+    completed: false,
+    index: 1,	
+    },
+  {
+    description: "cooking",
+    completed: false,
+    index: 2,
+    },
+]
+
+document.querySelector('.mylist').innerHTML = tasks.map((task) =>
+ `<li>
+    <div class="task-container">
+    <input type="checkbox" id="checkbox${task.index}">  
+    <h4>${task.description}</h4>
+    </div>
+  </li>`).join('');
